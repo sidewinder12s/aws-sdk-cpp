b5a192979e1640fdcd9a06b78905b21a46d8e377
Geoff Webster <1393426+sidewinder12s@users.noreply.github.com>
2020-06-01 12:40:19 -0700

    Add ProcessCredentialsProvider to default provider Chain

--- aws-cpp-sdk-core/source/auth/AWSCredentialsProviderChain.cpp
+++ aws-cpp-sdk-core/source/auth/AWSCredentialsProviderChain.cpp
@@ -46,6 +46,7 @@ DefaultAWSCredentialsProviderChain::DefaultAWSCredentialsProviderChain() : AWSCr
 {
     AddProvider(Aws::MakeShared<EnvironmentAWSCredentialsProvider>(DefaultCredentialsProviderChainTag));
     AddProvider(Aws::MakeShared<ProfileConfigFileAWSCredentialsProvider>(DefaultCredentialsProviderChainTag));
+    AddProvider(Aws::MakeShared<Aws::Auth::ProcessCredentialsProvider>(DEBUG_TAG));
     AddProvider(Aws::MakeShared<STSAssumeRoleWebIdentityCredentialsProvider>(DefaultCredentialsProviderChainTag));
     
     //ECS TaskRole Credentials only available when ENVIRONMENT VARIABLE is set
